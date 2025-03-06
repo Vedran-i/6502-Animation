d27c4da6c3c46a478e896381c44d1566db167d78
Vedran <vedde13@gmail.com>
2025-03-06 13:58:27 +0100

    Create animation5.asm

---
+++ animation5.asm
@@ -0,0 +1,41 @@
+Start:
+
+Code:
+
+LDA #$01
+STA $300, X
+JSR delay
+INX
+
+
+JMP Code
+
+delay:
+LDA #$00
+STA $1001
+LDA #$00
+STA $1002
+
+LDA #$30
+STA $2001
+LDA #$01
+STA $2002
+
+loop:
+LDA $1001
+CMP $2001
+BEQ innerloop
+loop1:
+INC $1001
+LDA $1002
+CMP $2002
+BEQ done
+JMP loop
+
+innerloop:
+INC $1002
+JMP loop1
+
+done:
+
+RTS
